dd01d62d8bd24817e2c83db0846e35f4de0eb016
cocopop1106 <zhzh1300>
2018-06-14 19:01:09 +0900

    createChannel

--- src/main/java/com/skcc/cloudz/zcp/alert/vo/ChannelVo.java
+++ src/main/java/com/skcc/cloudz/zcp/alert/vo/ChannelVo.java
@@ -2,21 +2,8 @@ package com.skcc.cloudz.zcp.alert.vo;
 
 public class ChannelVo {
 	
-	private String id;
 	private String channel;
 	
-	/**
-	 * @return the id
-	 */
-	public String getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(String id) {
-		this.id = id;
-	}
 	/**
 	 * @return the channel
 	 */
