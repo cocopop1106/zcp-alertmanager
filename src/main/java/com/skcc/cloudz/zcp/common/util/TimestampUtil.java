3a9bc8e2705a09ac1e413d5d3c52d00283b24281
cocopop1106 <zhzh1300>
2018-07-19 17:55:48 +0900

    iso8601 dateformat change

--- src/main/java/com/skcc/cloudz/zcp/common/util/TimestampUtil.java
+++ src/main/java/com/skcc/cloudz/zcp/common/util/TimestampUtil.java
@@ -1,17 +1,29 @@
 package com.skcc.cloudz.zcp.common.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.Date;
+
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
 
 public class TimestampUtil {
 	
 	public static String ISO8601ToDate(Object startsAt) {
 		
-		java.util.Date date = Date.from( Instant.parse((CharSequence) startsAt));
+		ISO8601DateFormat df = new ISO8601DateFormat();
+		String dateFormat = "";
+		try {
+			Date d = df.parse(startsAt.toString());
+			
+			SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+			dateFormat = sDateFormat.format(d);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 		
-		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
-		String dateFormat = df.format(date);
+//		java.util.Date date = Date.from( Instant.parse((CharSequence) startsAt));
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
+//		String dateFormat = df.format(date);
 		
 		return dateFormat;
 	}
